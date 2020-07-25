e31600e10fc3992035dca71f22fae21919d0f24f
Joey Petrone <joey.petrone@gmail.com>
2020-07-25 15:11:32 -0500

    Add project files.

---
+++ Variables_MoshYouTubeVideo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables_MoshYouTubeVideo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
