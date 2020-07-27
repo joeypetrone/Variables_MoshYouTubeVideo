94770f4c37f0fdcc0b70bf3f51ef2930bad8b269
Joey Petrone <joey.petrone@gmail.com>
2020-07-27 18:02:38 -0500

    Const value error

--- Variables_MoshYouTubeVideo/Program.cs
+++ Variables_MoshYouTubeVideo/Program.cs
@@ -6,8 +6,8 @@ namespace Variables_MoshYouTubeVideo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Pi = 1;
         }
     }
 }
